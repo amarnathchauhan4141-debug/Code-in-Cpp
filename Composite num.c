b45e0e2fad71ed9f3ce120dd4ff6ba986f04450d
amarnathchauhan4141-debug <amarnathchauhan4141@gmail.com>
2026-05-31 17:12:28 +0530

    Add files via upload

---
+++ Composite num.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("Enter the numbers:");
+	scanf("%d",&n);
+	for (int i =2; i<=n-1;i++)
+	{    
+	    if (n%i==0)
+		printf("The given number is composite number:");
+		break;
+	}
+	
+	
+}
